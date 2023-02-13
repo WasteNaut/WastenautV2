9e740cb8afd5a3710e6964b19ac09ad290623efd
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-02-13 12:07:47 -0800

    added userContext and Header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Header from './Components/Header.js';
+import { useUser } from './Components/context/UserContext.js';
+import Auth from './Components/Auth/Auth.js';
 
 function App() {
-  return (
-    <div className="App">
-      Wastenaut
-    </div>
-  );
+    const { user } = useUser();
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route exact path='/auth/:type' component={Auth} />
+                <Route exact path='/'>
+                    <>
+                        {user && <Navigate to='/list' />}
+                        {!user && <Navigate to='/auth/sign-in' />}
+                    </>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
