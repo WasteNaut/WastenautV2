7181f978682533b2ba30e849d2e793979ceaef35
brenley-woodard <brenleyew@gmail.com>
2023-02-13 10:33:47 -0800

    downloaded react router, added BrowserRouter to Index, and added state and form to Auth componenet

--- my-app/src/Components/Auth/Auth.js
+++ my-app/src/Components/Auth/Auth.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { Form, useParams } from 'react-router-dom';
+import { authUser } from '../../Services/auth';
 
 export default function Auth() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const { type } = useParams();
+  // const { user, setUser } = useContext(UserContext);
+
+  const submitAuth = async (e) => {
+    e.preventDefault();
+
+    // try {
+    //   const newUser = await authUser(email, password, type);
+    //   setUser(newUser);
+    // } catch (e) {
+    //   console.error(e);
+    // }
+  };
+
   return (
-    <div>Auth</div>
-  )
+    <div>
+      <form onSubmit={submitAuth}>
+        <label>Email</label>
+        <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>
+          Submit
+        </button>
+      </form>
+
+    </div>
+  );
 }
